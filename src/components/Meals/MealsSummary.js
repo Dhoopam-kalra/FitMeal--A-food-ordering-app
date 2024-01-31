75caf4a62cb9367ddcbfa282e0d0fefcd63285ce
Dhoopam-kalra <dhoopamkalra@gmail.com>
2024-01-31 12:56:08 +0530

    Initial Commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import classes from './MealsSummary.module.css';
+const MealsSummary = () => {
+ return <section className={classes.summary}>
+    <h2>Delicious Food, Delivered At Your DoorStep!</h2>
+    <p>
+        Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch and dinner with fast delivery at your home.
+    </p>
+    <p>
+        All of our meals are cooked with high quality ingredients by our experienced chefs!
+    </p>
+ </section>
+};
+export default MealsSummary;
